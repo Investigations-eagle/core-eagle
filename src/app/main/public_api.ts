7784a9378baee13cecb64119af34c9f1ef11e7a2
Vladymyr Shevchuk <dosankv@gmail.com>
2018-04-17 22:26:07 +0300

    Fixed import in public_api.ts

--- src/app/main/public_api.ts
+++ src/app/main/public_api.ts
@@ -1,5 +1,3 @@
-import {LoginComponent} from "./login/login.component";
-
 export { CoreModule } from './core/core.module';
 export { LoginComponent } from './login/login.component';
 export { MenuComponent } from './menu/menu.component';
